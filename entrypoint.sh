05c79644f4610ca8d2da1346c4f7bc7bde8557a6
umiyosh <yumino99@gmail.com>
2019-02-21 18:58:42 +0900

    enable to specify error rate

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ envsubst \
 '\$WORKER_CONNECTIONS \$WORKER_PROCESSES' \
   < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 
-envsubst '\$UPSTREAM_NAME \$UPSTREAM_KEEPALIVE \$SERVER_NAME' < /etc/nginx/nginx.vh.template.default.conf > /etc/nginx/conf.d/default.conf
+envsubst '\$UPSTREAM_NAME \$UPSTREAM_KEEPALIVE \$SERVER_NAME \$ERROR_RATE' < /etc/nginx/nginx.vh.template.default.conf > /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
