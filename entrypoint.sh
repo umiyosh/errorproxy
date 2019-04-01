4f0f29888aaf761cf03b38145bdddd05634e12ee
umiyosh <yumino99@gmail.com>
2019-04-01 14:28:23 +0900

    added latency parameter LATENCY_OK_MS,  LATENCY_ERR_MS

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ envsubst \
 '\$WORKER_CONNECTIONS \$WORKER_PROCESSES' \
   < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 
-envsubst '\$LISTEN_PORT \$UPSTREAM_NAME \$UPSTREAM_KEEPALIVE \$SERVER_NAME \$ERROR_RATE' < /etc/nginx/nginx.vh.template.default.conf > /etc/nginx/conf.d/default.conf
+envsubst '\$LATENCY_ERR_MS \$LATENCY_OK_MS \$LISTEN_PORT \$UPSTREAM_NAME \$UPSTREAM_KEEPALIVE \$SERVER_NAME \$ERROR_RATE' < /etc/nginx/nginx.vh.template.default.conf > /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
